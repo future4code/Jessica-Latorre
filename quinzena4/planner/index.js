8cf2c9be0b3a86b9fec75838ad5f3974ba497c12
Jessica Latorre <jessica.latorre182@gmail.com>
2021-04-17 20:25:34 -0300

    projeto Planner completo

--- quinzena4/planner/index.js
+++ quinzena4/planner/index.js
@@ -1,4 +1,4 @@
-
+let contadorDeTarefa = 0
 
 function adicionarTarefa(){
     let tarefa = document.getElementById("tarefa").value
@@ -7,15 +7,30 @@ function adicionarTarefa(){
         return
     }
     let diaSemana = document.getElementById("dias-semana").value
-
+    let idTarefa = `tarefa-${contadorDeTarefa}`
+    let horarioTarefa = confirm("Deseja adicionar um horário a esta tarefa?")
+    if (horarioTarefa === true){
+        let horario = prompt("Adicione um horário para esta tarefa. (ex.: 00h, 13h)")// adiciona horario as tarefas criadas.
+        document.getElementById(diaSemana).innerHTML += `<p id=${idTarefa} onclick='riscarTarefa("${idTarefa}")'>- ` + tarefa + ` início às: `+ horario + ` </p>`
+        document.getElementById("tarefa").value = ""
+    } else{
+        document.getElementById(diaSemana).innerHTML += `<p>- ` + tarefa + ` </p>`
+        document.getElementById("tarefa").value = ""
+    }
     
-    document.getElementById(diaSemana).innerHTML += `<p>- ` + tarefa +` </p>`
-    document.getElementById("tarefa").value = ""
-
-}  
-
-function limparTarefas(){ // só funciona se selecionar o dia da semana das tarefas que deseja limpar.
-    let diaSemana = document.getElementById("dias-semana").value
-    document.getElementById(diaSemana).innerHTML = ""
-   
 }
+function riscarTarefa(id){
+    let tarefa = document.getElementById(id)
+    tarefa.innerHTML = `<p class="tarefa-riscada">${tarefa.innerHTML}</p>`
+}
+
+
+function limparTarefas(){ // Agora funciona normalmente, conforme o enunciado do desafio.
+    let diasDaSemana = document.querySelectorAll(".semana > div")
+    for(let index of diasDaSemana){
+        index.innerHTML = ""
+    }
+    
+    }
+    
+   
